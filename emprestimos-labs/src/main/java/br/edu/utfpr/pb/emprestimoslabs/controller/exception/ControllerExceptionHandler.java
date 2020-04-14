193b4ce2f520b9a4727697feec62b6c68954e366
Luiz Gustavo <oluizgustavob@gmail.com>
2020-04-14 20:16:23 -0300

    update spring

--- emprestimos-labs/src/main/java/br/edu/utfpr/pb/emprestimoslabs/controller/exception/ControllerExceptionHandler.java
+++ emprestimos-labs/src/main/java/br/edu/utfpr/pb/emprestimoslabs/controller/exception/ControllerExceptionHandler.java
@@ -26,8 +26,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import jdk.nashorn.internal.objects.annotations.Getter;
-
 @RestControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
